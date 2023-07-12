fd3f70765997fb5ad76388cbee47776f2158dbd2
kostiaserdiuk <kostiaserdyk@gmail.com>
2023-07-13 02:15:59 +0300

    BE-3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from .db.db import engine, r
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await engine.connect()
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await engine.dispose()
 
 @app.get("/")
 def health_check():
